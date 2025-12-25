acf51197d7c93a38123c39bb3bb0e414bdf0143b
fuwaguwa <flufftehgamer@gmail.com>
2025-12-25 17:59:18 +0700

    ref: environment files

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,11 +1,9 @@
 import { ApplicationCommandRegistries, RegisterBehavior } from "@sapphire/framework";
-import { config } from "dotenv";
-import { join } from "path";
 import "@sapphire/plugin-logger/register";
 import "@sapphire/plugin-subcommands/register";
+import { config } from "dotenv";
 
-const env = process.env.NODE_ENV || "development";
-config({ path: join(process.cwd(), `.env.${env}`) });
+config({ path: `.env.${process.env.NODE_ENV}` });
 
 // Set default behavior for slash commands
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
