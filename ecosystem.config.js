2dbf23c947af05926cc155365ad2d368c80ab7f2
fuwaguwa <flufftehgamer@gmail.com>
2025-12-25 16:48:45 +0700

    ref: changed pm2 name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [{
-    name: 'your-app-name',
-    script: 'bun',
-    args: 'run prod',
+    name: "Shinano",
+    script: "bun",
+    args: "run prod",
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: "production"
     },
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '4G'
+    max_memory_restart: "4G"
   }]
 }
