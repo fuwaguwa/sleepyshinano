b687156d2b14a5a190099de62d8b4d4298101493
fuwaguwa <flufftehgamer@gmail.com>
2025-12-27 18:37:01 +0700

    refactor: not rethrowing errors

--- src/listeners/guild/guildCreate.ts
+++ src/listeners/guild/guildCreate.ts
@@ -4,28 +4,22 @@ import { updateServerCount } from "../../lib/utils/logging";
 
 export class GuildCreateListener extends Listener<typeof Events.GuildCreate> {
   public override async run(guild: Guild) {
-    try {
-      const auditLog = await guild.fetchAuditLogs({
-        type: AuditLogEvent.BotAdd,
-        limit: 1,
-      });
+    const auditLog = await guild.fetchAuditLogs({
+      type: AuditLogEvent.BotAdd,
+      limit: 1,
+    });
 
-      const entry = auditLog.entries.first();
-      const adder = entry?.executor;
+    const entry = auditLog.entries.first();
+    const adder = entry?.executor;
 
-      if (adder) {
-        this.container.logger.info(
-          `[${guild.shard.id}] New Guild: ${guild.name} (${guild.id}) - Added by: ${adder.username} (${adder.id})`
-        );
-      } else {
-        this.container.logger.info(
-          `[${guild.shard.id}] New Guild: ${guild.name} (${guild.id})`
-        );
-      }
-
-      await updateServerCount();
-    } catch (error) {
-      throw error;
+    if (adder) {
+      this.container.logger.info(
+        `[${guild.shard.id}] New Guild: ${guild.name} (${guild.id}) - Added by: ${adder.username} (${adder.id})`
+      );
+    } else {
+      this.container.logger.info(`[${guild.shard.id}] New Guild: ${guild.name} (${guild.id})`);
     }
+
+    await updateServerCount();
   }
 }
