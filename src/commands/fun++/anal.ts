fe00df051e70d04404fff2e5e63fb50cb92c7e20
fuwaguwa <flufftehgamer@gmail.com>
2026-01-01 17:31:24 +0700

    feat: more booru command

---
+++ src/commands/fun++/anal.ts
@@ -0,0 +1,26 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Command, type CommandOptions } from "@sapphire/framework";
+import { processBooruRequest } from "../../lib/booru";
+
+@ApplyOptions<CommandOptions>({
+  description: "The other hole.",
+  fullCategory: ["NSFW", "Booru"],
+  cooldownLimit: 1,
+  cooldownDelay: 15000,
+  cooldownFilteredUsers: process.env.COOL_PEOPLE_IDS.split(",") || [],
+  preconditions: ["NotBlacklisted"],
+  nsfw: true,
+})
+export class GelbooruCommand extends Command {
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand(builder =>
+      builder.setName(this.name).setDescription(this.description).setNSFW(true)
+    );
+  }
+
+  public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    if (!interaction.deferred) await interaction.deferReply();
+
+    await processBooruRequest({ interaction, tags: "anal -yaoi", site: "gelbooru", noTagsOnReply: true });
+  }
+}
