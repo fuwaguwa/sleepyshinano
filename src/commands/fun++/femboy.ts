c34f0fb52d5005fcaec5fb1398510a18e73f5efa
fuwaguwa <flufftehgamer@gmail.com>
2026-01-01 17:32:11 +0700

    refactor: recategorized commands

---
+++ src/commands/fun++/femboy.ts
@@ -0,0 +1,36 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Command, type CommandOptions } from "@sapphire/framework";
+import { ApplicationIntegrationType, InteractionContextType } from "discord.js";
+import { processBooruRequest } from "../../lib/booru";
+
+@ApplyOptions<CommandOptions>({
+  description: "Due to popular demands...",
+  fullCategory: ["NSFW", "Booru"],
+  cooldownLimit: 1,
+  cooldownDelay: 15000,
+  cooldownFilteredUsers: process.env.COOL_PEOPLE_IDS.split(",") || [],
+  preconditions: ["NotBlacklisted"],
+  nsfw: true,
+})
+export class GelbooruCommand extends Command {
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand(builder =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .setNSFW(true)
+        .setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall])
+        .setContexts([
+          InteractionContextType.Guild,
+          InteractionContextType.BotDM,
+          InteractionContextType.PrivateChannel,
+        ])
+    );
+  }
+
+  public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    if (!interaction.deferred) await interaction.deferReply();
+
+    await processBooruRequest({ interaction, tags: "trap", site: "rule34", noTagsOnReply: true });
+  }
+}
---
+++ src/commands/image/uma.ts
@@ -0,0 +1,34 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Command, type CommandOptions } from "@sapphire/framework";
+import { processBooruRequest } from "../../lib/booru";
+import { ApplicationIntegrationType, InteractionContextType } from "discord.js";
+
+@ApplyOptions<CommandOptions>({
+  description: "Horse girls",
+  cooldownLimit: 1,
+  cooldownDelay: 15000,
+  cooldownFilteredUsers: process.env.COOL_PEOPLE_IDS.split(",") || [],
+  preconditions: ["NotBlacklisted"],
+  nsfw: true,
+})
+export class GelbooruCommand extends Command {
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand(builder =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall])
+        .setContexts([
+          InteractionContextType.Guild,
+          InteractionContextType.BotDM,
+          InteractionContextType.PrivateChannel,
+        ])
+    );
+  }
+
+  public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    if (!interaction.deferred) await interaction.deferReply();
+
+    await processBooruRequest({ interaction, tags: "umamusume", site: "safebooru", noTagsOnReply: true });
+  }
+}
