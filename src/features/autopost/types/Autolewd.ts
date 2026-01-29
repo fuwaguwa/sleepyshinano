63d5b09e84468d8d9ff6fe39bb4b4bbec6274a6b
fuwaguwa <flufftehgamer@gmail.com>
2026-01-29 10:42:27 +0700

    chore(autopost): update import paths

--- src/features/autopost/types/Autolewd.ts
+++ src/features/autopost/types/Autolewd.ts
@@ -1,6 +1,6 @@
 import type { ButtonInteraction, ChatInputCommandInteraction, ContainerBuilder, Message } from "discord.js";
 import type { Types } from "mongoose";
-import type { LewdCategory } from "../../private-lewd/types/Lewd";
+import type { LewdCategory } from "../../private/types/Lewd";
 
 export interface AutolewdButtonOptions {
   showEnable: boolean;
